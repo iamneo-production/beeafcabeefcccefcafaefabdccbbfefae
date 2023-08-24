de91479c37106988369ea11da37195ef9bf7fe09
Iamneo <techadmin@iamneo.ai>
2023-08-24 05:54:06 +0000

    Backup Commit

---
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -0,0 +1,68 @@
+package com.examly.springapp;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@SpringBootTest(classes = SpringappApplication.class)
+@AutoConfigureMockMvc
+class SpringappApplicationTests {
+
+	@Autowired
+	private  MockMvc mockMvc ;
+
+	@Test
+	void addlabRecords() throws Exception{	
+
+		String st = "{\"laptopId\": 1000, \"laptopPrice\": 30000,\"laptopBrand\": \"dell\"}";
+		 mockMvc.perform(MockMvcRequestBuilders.post("/")
+		 				.contentType(MediaType.APPLICATION_JSON)
+		 				.content(st)
+		 				.accept(MediaType.APPLICATION_JSON))
+						.andExpect(MockMvcResultMatchers.status().isOk())
+						.andExpect(jsonPath("$").value(true))
+						.andReturn();
+	}
+
+	@Test
+	void getById() throws Exception{	
+
+		 mockMvc.perform(get("/1000")
+		 				.accept(MediaType.APPLICATION_JSON))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.laptopPrice").value("30000"))
+						.andReturn();
+	}
+
+	
+
+	@Test
+	void getAll() throws Exception{	
+
+		 mockMvc.perform(get("/")
+		 				.accept(MediaType.APPLICATION_JSON))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$").isArray())
+						.andReturn();
+	}
+
+	
+
+}
